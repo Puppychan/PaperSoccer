ec013d793780f72116bafa7278dc7f85fa09e336
Matsuri <nhungmaitran1412@gmail.com>
2022-08-19 14:17:07 +0700

    Initial Commit

---
+++ Shared/PaperSoccerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperSoccerApp.swift
+//  Shared
+//
+//  Created by Nhung Tran on 19/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct PaperSoccerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
